175c559a93b5db77a47673a7287ed59b58622f5e
sofyagubanova <sonyg-home@mail.ru>
2017-06-04 06:22:42 +0700

    Update quest.php

--- quest.php
+++ quest.php
@@ -1,12 +1,12 @@
 <?require 'head.php';?>
-   <body>
-      <div class="main-block">
-         <div class="container paper">
-            <?require 'header.php';?>
-            <div id="main">
-                <div id="contacts">
-                  <h2 class="post_ttl2">Гостевая книга</h2>
-				  <div id="quest" style="background:none;">
+<body>
+   <div class="main-block">
+      <div class="container paper">
+         <?require 'header.php';?>
+         <div id="main">
+            <div id="contacts">
+               <h2 class="post_ttl2">Гостевая книга</h2>
+               <div id="quest" style="background:none;">
                   <div style="margin:40px; text-align:justify;">
                      <form action="add_mail.php" method="post">
                         <p><b>Хотите оставить нам свое пожелание или сообщение в гостевой книге?</b></p>
@@ -18,26 +18,25 @@
                         <p><input style="margin-top:20px; font-size: 22px; width:150px; height:50px;" type="submit" value="Отправить"></p>
                      </form>
                   </div>
-                  <?	
-					 $names=file('files/quest_name.txt', FILE_IGNORE_NEW_LINES);
-                     $mails=file('files/quest_text.txt', FILE_IGNORE_NEW_LINES);
-                     $mails_dates=file('files/quest_date.txt', FILE_IGNORE_NEW_LINES);
-                     $mails_times=file('files/quest_time.txt', FILE_IGNORE_NEW_LINES);
-                     for ($i=0; $i<count($names); $i++)
-                     {
-                     	echo '<div id="addmail">
-                     <p><b>',$names[$i],' (',$mails_dates[$i],' ',$mails_times[$i],')</b></p>
-                     <p>',$mails[$i],'</p>
-                     </div>';
-                     }
-                     
-                     ?>
+                  <?
+			$names       = file('files/quest_name.txt', FILE_IGNORE_NEW_LINES);
+			$mails       = file('files/quest_text.txt', FILE_IGNORE_NEW_LINES);
+			$mails_dates = file('files/quest_date.txt', FILE_IGNORE_NEW_LINES);
+			$mails_times = file('files/quest_time.txt', FILE_IGNORE_NEW_LINES);
+			for ($i = 0; $i < count($names); $i++) {
+			    echo '<div id="addmail">
+				<p><b>', $names[$i], ' (', $mails_dates[$i], ' ', $mails_times[$i], ')</b></p>
+				<p>', $mails[$i], '</p>
+				 </div>';
+			}
+
+		?>
                </div>
-			   </div>
             </div>
-            <div class="clear-both"></div>
          </div>
+         <div class="clear-both"></div>
       </div>
-      <?require 'footer.php';?>
-   </body>
+   </div>
+   <?require 'footer.php';?>
+</body>
 </html>
