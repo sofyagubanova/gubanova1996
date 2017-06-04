2b07d8e4707391286d473fbbfa1a2bef5b25928b
sofyagubanova <sonyg-home@mail.ru>
2017-06-04 16:34:44 +0700

    Update register.php

--- register.php
+++ register.php
@@ -7,26 +7,29 @@
             <div id="catalog">
                <h2><a class="post_ttl">Успешная регистрация</a></h2>
                <div id="basket" style="background:none;">
-                  <?php 
-					$login=htmlspecialchars($_POST['login']);
-					$password=htmlspecialchars($_POST['password']);
-					$logins=file('files/loginsJ72hds93jSHW82q.txt', FILE_IGNORE_NEW_LINES);
+                  <?php
+		       if (isset($_SESSION['login'])) {
+				echo '<script>location.href="index.php"</script>';
+			}
+			else {
+				$login=htmlspecialchars($_POST['login']);
+				$password=htmlspecialchars($_POST['password']);
+				$logins=file('files/loginsJ72hds93jSHW82q.txt', FILE_IGNORE_NEW_LINES);
 					
-					for ($i=0; $i<count($logins); $i++)
-					{
-						if ($login==$logins[$i])
-						{
-							$_SESSION['login_already_exist']=true;
-							echo '<script>location.href="registration.php"</script>';
-							exit;
-						}
+				for ($i=0; $i<count($logins); $i++){
+					if ($login==$logins[$i]){
+						$_SESSION['login_already_exist']=true;
+						echo '<script>location.href="registration.php"</script>';
+						exit;
 					}
+				}
 					
-					file_put_contents('files/loginsJ72hds93jSHW82q.txt', $login."\n", FILE_APPEND);
-					file_put_contents('files/passwordsQ78F2hd82saFWAZ212.txt', $password."\n", FILE_APPEND);
-					$_SESSION['login']=$login;
-					echo '<p align="center" style="font-size:24px;color:#fff;font-weight:bold;"> Вы успешно зарегистрированы!</p>';
-					?>
+				file_put_contents('files/loginsJ72hds93jSHW82q.txt', $login."\n", FILE_APPEND);
+				file_put_contents('files/passwordsQ78F2hd82saFWAZ212.txt', $password."\n", FILE_APPEND);
+				$_SESSION['login']=$login;
+				echo '<p align="center" style="font-size:24px;color:#fff;font-weight:bold;"> Вы успешно зарегистрированы!</p>';
+			}
+		       ?>
                </div>
             </div>
          </div>
